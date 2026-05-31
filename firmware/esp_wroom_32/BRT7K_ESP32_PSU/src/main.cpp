f60a66620588e88524702e8b66240f4655560daa
jan <jan.wild@stud.fhgr.ch>
2026-05-31 15:25:58 +0200

    edit motor id

--- firmware/esp_wroom_32/BRT7K_ESP32_PSU/src/main.cpp
+++ firmware/esp_wroom_32/BRT7K_ESP32_PSU/src/main.cpp
@@ -44,7 +44,7 @@
 #define MOTOR_RIGHT_RX   16
 #define MOTOR_RIGHT_TX   17
 #define MOTOR_BAUD       115200
-#define LEFT_ID          0x01
+#define LEFT_ID          0x02
 #define RIGHT_ID         0x01
 #define MAX_RPM          210.0f
 
