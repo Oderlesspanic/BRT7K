499b48a499a13cc57e56151b2464561567fc0304
jan <jan.wild@stud.fhgr.ch>
2026-06-04 11:28:55 +0200

    Remove magnetometer from IMU odometry pipeline

--- ros2_ws/src/robot_bringup/launch/odometry.launch.py
+++ ros2_ws/src/robot_bringup/launch/odometry.launch.py
@@ -13,20 +13,6 @@ def generate_launch_description():
         "imu.launch.py"
     )
 
-    mmc5603_dir = get_package_share_directory("mmc5603")
-    mmc5603_launch = os.path.join(
-        mmc5603_dir,
-        "launch",
-        "mmc5603.launch.py"
-    )
-
-    imu_mag_fusion_dir = get_package_share_directory("imu_mag_fusion")
-    imu_mag_fusion_launch = os.path.join(
-        imu_mag_fusion_dir,
-        "launch",
-        "madgwick_with_cov_fix.launch.py"
-    )
-
     ekf_dir = get_package_share_directory("ekf")
     ekf_launch = os.path.join(
         ekf_dir,
@@ -38,12 +24,6 @@ def generate_launch_description():
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(imu_launch)
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(mmc5603_launch)
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(imu_mag_fusion_launch)
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(ekf_launch)
